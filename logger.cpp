af8667a9f85e666d985ff99f33ba935e76dc920b
phensd <ngnf@mail.com>
2025-01-03 22:42:52 -0600

    Make logging to cerr a little prettier

--- logger.cpp
+++ logger.cpp
@@ -1,6 +1,5 @@
 #include "include/logger.h"
 #include <filesystem>
-#include <fstream>
 #include <iostream>
 
 bool comet::logger::log(const std::string& message,bool output_to_cerr){
@@ -8,19 +7,20 @@ bool comet::logger::log(const std::string& message,bool output_to_cerr){
     //just remove the file if its bigger than 10mb
     if(std::filesystem::exists(output_file_path) && std::filesystem::file_size(output_file_path) > 10000000) std::filesystem::remove(output_file_path);
 
-    std::ofstream output_file {output_file_path,std::ofstream::app};
+    // std::ofstream output_file {output_file_path,std::ofstream::app};
 
     //this will make the window briefly flash to the terminal 
     //this is desirable (imo) when the song paths are being scanned, 
     //then the user can see what is going on if it is taking a while
     //otherwise not.
     //this is only used for that instance
-    if(output_to_cerr) std::cerr << message << '\n';
+    auto time_t = std::time(nullptr);
+    if(output_to_cerr) std::cerr << "[comet " << std::put_time(std::localtime(&time_t),"%B %d %H:%M:%S") << "] "<< message << '\n';
 
 
-    output_file << message << '\n';
+    // output_file << message << '\n';
 
-    output_file.close();
+    // output_file.close();
 
     return true;
 }
